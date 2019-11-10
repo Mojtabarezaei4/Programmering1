5e940804b9c48c71a0ec3c80d81c7a69a01dca05
mojtabarezaei4 <mojtaba.rezaei@elev.ga.ntig.se>
2019-11-10 09:59:19 +0100

    Hej första metoden är avklarad.

--- FysikLib/src/com/company/Main.java
+++ FysikLib/src/com/company/Main.java
@@ -4,5 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println(fahrenheitToCelsius(50));
+
+
     }
+
+    /**
+     *
+     * @param fahrenheit - The temperature at Fahrenheit
+     * @return It returns the temperature at Celsius
+     */
+
+    public static double fahrenheitToCelsius(double fahrenheit){
+
+        double celsius = 0;
+        celsius = ((fahrenheit-32)*5)/9;
+
+        return celsius;
+
+    }
+
+
+
 }
