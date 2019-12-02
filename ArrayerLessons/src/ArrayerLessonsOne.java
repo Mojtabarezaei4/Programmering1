d39a6ebd1bacbbf2feb1b63f2cb7e291bb8c5815
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2019-12-02 10:51:48 +0100

    Forsta arrayer-lista lektion.

---
+++ ArrayerLessons/src/ArrayerLessonsOne.java
@@ -0,0 +1,48 @@
+public class ArrayerLessonsOne {
+
+    public static void main(String[] args) {
+
+    /*
+        int[] arr = {1, 2, 3};
+
+        int[] arr2 = new int[5];
+        arr2[0] = 1;
+        arr2[1] = 2;
+        arr2[2] = 3;
+        arr2[3] = 4;
+        arr2[4] = 5;
+
+
+
+        int[] femmansxtabel = new int[20];
+        for (int i = 0; i <= 20; i ++) {
+
+            femmansxtabel[i] = i * 5;
+        }
+
+        System.out.println(femmansxtabel[3]);
+
+        for (int i = 0; i < femmansxtabel.length -1; i++){
+
+            System.out.println(femmansxtabel[i]);
+        }
+
+        for (int fiver: femmansxtabel) {
+            System.out.println(fiver);
+        }
+
+        */
+        int [] sjuan = new int[1000];
+        for (int i = 0; i < 1000; i++){
+
+            sjuan[i] = i * 7;
+
+        }
+
+        for (int sevens: sjuan) {
+
+            System.out.println(sevens);
+
+        }
+    }
+}
