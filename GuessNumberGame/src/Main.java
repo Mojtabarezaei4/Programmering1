7f93dedcb3d8736d05462972e4084ef3eadc6e23
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-03-14 23:03:16 +0100

    I just made the methods and wrote the Java-Doc ;)

--- GuessNumberGame/src/Main.java
+++ GuessNumberGame/src/Main.java
@@ -11,4 +11,39 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    /**
+     *
+     * This method contains the code of the game.
+     * This will contain the whole game.
+     */
+    protected static void launchGuesseyGame(){
+
+    }
+
+    /**
+     *
+     * This method contains the easiest mode of this game.
+     */
+    protected static void easyMode(){
+
+    }
+
+    /**
+     *
+     * This method contains the medium mode of this game.
+     * This will complain how this mode works.
+     */
+    protected static void mediumMode(){
+
+    }
+
+    /**
+     *
+     * This method contains the hardest mode of this game.
+     * This says how the game plays and so on.
+     */
+    protected static void hardMode{
+
+    }
 }
