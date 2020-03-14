52fa0aef8f7ed1205ccd841ae472bd97a2a60ac5
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-03-14 22:53:18 +0100

    I just made my variables

--- GuessNumberGame/src/Main.java
+++ GuessNumberGame/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 
+    protected int theMinimumNumber;     //the minimum number in the interval
+    protected int theHighestNumber;     //the highest number in the interval
+    protected int theRightAnswer;       //the randomized number
+    protected int theGuessesLeft;       //the number of guesses which are left
+    protected int theNumberOfGuesses;   //the number of guesses which the player choose
+    protected int theGuessedNumber;     //the number which the player guessed
+
+
     public static void main(String[] args) {
 
     }
