5b5d4cb69d999d39e1249ae564a642f52fa8d477
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-03-15 00:45:26 +0100

    I made a randomize method for make an answer then made the easyMode();

--- GuessNumberGame/src/Main.java
+++ GuessNumberGame/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -5,17 +6,30 @@ public class Main {
     static Scanner _inputFromThePlayer = new Scanner(System.in);
 
     protected static int _chooseTheLevel;       //the level which the player chooses
-    protected int _theMinimumNumber;     //the minimum number in the interval
-    protected int _theHighestNumber;     //the highest number in the interval
-    protected int _theRightAnswer;       //the randomized number
-    protected int _theGuessesLeft;       //the number of guesses which are left
-    protected int _theNumberOfGuesses;   //the number of guesses which the player choose
-    protected int _theGuessedNumber;     //the number which the player guessed
+    protected static int _theMinimumNumber;     //the minimum number in the interval
+    protected static int _theHighestNumber;     //the highest number in the interval
+    protected static int _theRightAnswer;       //the randomized number
+    protected static int _theGuessesLeft;       //the number of guesses which are left
+    protected static int _theNumberOfGuesses;   //the number of guesses which the player choose
+    protected static int _theGuessedNumber;     //the number which the player guessed
 
 
     public static void main(String[] args) {
 
         launchGuesseyGame();
+    }
+
+    /**
+     * This method makes the answer.
+     * @param _theHighestNumber the highest number in the interval
+     * @param _theMinimumNumber the minimum number in the interval
+     * @return the number which the player has to guess
+     */
+    protected static int theRightAnswer(int _theHighestNumber, int _theMinimumNumber){
+        Random theAnswer = new Random();
+
+        return theAnswer.nextInt(_theHighestNumber - _theMinimumNumber +1 ) +
+                _theMinimumNumber;
     }
 
     /**
@@ -65,10 +79,6 @@ public class Main {
                     break;
             }
 
-
-
-
-
     }
 
     /**
@@ -76,6 +86,59 @@ public class Main {
      * This method contains the easiest mode of this game.
      */
     protected static void easyMode(){
+
+        System.out.println("How many guesses do you want?");
+        _theNumberOfGuesses = _inputFromThePlayer.nextInt();
+        System.out.println("Write the minimum number of the interval which you " +
+                "want to play between: ");
+        _theMinimumNumber = _inputFromThePlayer.nextInt();
+        System.out.println("Write the highest number of the interval which you " +
+                "want to play between: ");
+        _theHighestNumber = _inputFromThePlayer.nextInt();
+
+        System.out.println("You choosed a interval between " + _theMinimumNumber +
+                "- " + _theHighestNumber + "and you have " + _theNumberOfGuesses +
+                " guesses to guess the number.");
+
+        System.out.println("Pleas try your first try ");
+
+        _theRightAnswer = theRightAnswer(_theHighestNumber, _theMinimumNumber);
+
+        System.out.println(_theRightAnswer);
+
+        for(int i = 1; i <= _theNumberOfGuesses; i--){
+
+            _theGuessedNumber = _inputFromThePlayer.nextInt();
+            _theNumberOfGuesses--;
+
+            if (_theGuessedNumber == _theRightAnswer){
+                System.out.println("Congurats!!!\n" +
+                        "You just won the game!\n" +
+                        "You are so smart!");
+                break;
+            }
+            else if (_theNumberOfGuesses != 0) {
+
+                if (_theGuessedNumber > _theRightAnswer) {
+                    System.out.println("You guessed wrong :( \n" +
+                            "Don't give up! Try a lower number :) \n" +
+                            "You have " + _theNumberOfGuesses + "guesses left");
+                }
+                else if (_theGuessedNumber < _theRightAnswer) {
+                   System.out.println("You guessed wrong :( \n" +
+                            "Don't give up! Try a higher number :) \n" +
+                            "You have " + _theNumberOfGuesses + "guesses left");
+
+                }
+            }
+            else{
+                System.out.println("It's so sad,\n" +
+                        "You lost.\n" +
+                        "But don't give up, you can win next time.");
+
+                break;
+            }
+        }
 
     }
 
@@ -85,6 +148,7 @@ public class Main {
      * This will complain how this mode works.
      */
     protected static void mediumMode(){
+
 
     }
 
